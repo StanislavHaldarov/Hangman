7a566edd6d82d30e017780722fe3d8a9de116d74
Stasko04 <stanislavhaldarov@gmail.com>
2022-06-15 20:01:50 +0300

    Commit 3

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Здравей");
-        System.out.print("!");
+        System.out.print("БЕСЕНИЦА");
     }
 }
